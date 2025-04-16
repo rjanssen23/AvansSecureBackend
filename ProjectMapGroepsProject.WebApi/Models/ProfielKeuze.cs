257b447f4fc61633f28abbd4ea263e62839a097f
rjanssen23 <r.janssen23@student.avans.nl>
2025-04-16 02:25:56 +0200

    Onnodige dingen verwijderd

--- ProjectMapGroepsProject.WebApi/Models/ProfielKeuze.cs
+++ ProjectMapGroepsProject.WebApi/Models/ProfielKeuze.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,14 +12,6 @@ namespace ProjectMap.WebApi.Models
         [StringLength(50)]
         public string? Name { get; set; }
 
-        [Required]
-        public string? Arts { get; set; }
-
-        [Required]
-        public int? GeboorteDatum { get; set; }
-
-        [Required]
-        public string? Avatar { get; set; }
 
         [ForeignKey("UserId")]
         public Guid UserId { get; set; } // Navigatie-eigenschap
